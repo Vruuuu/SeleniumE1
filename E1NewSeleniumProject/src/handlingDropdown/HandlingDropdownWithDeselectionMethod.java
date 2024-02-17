9ef80463d83e14780bdb4594880d7635d82d25e5
DELL <DELL@DESKTOP-E6GKMT9>
2024-02-17 19:14:38 +0530

    Popups And Executejavascript

--- E1NewSeleniumProject/src/handlingDropdown/HandlingDropdownWithDeselectionMethod.java
+++ E1NewSeleniumProject/src/handlingDropdown/HandlingDropdownWithDeselectionMethod.java
@@ -1,14 +1,12 @@
 package handlingDropdown;
 
 import java.time.Duration;
-import java.util.Iterator;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Sleeper;
+
 
 public class HandlingDropdownWithDeselectionMethod {
 
